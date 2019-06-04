0c6cdc557981e8ceeb1902ad43bbca0bed2a1818
Marnix0810 <48680596+Marnix0810@users.noreply.github.com>
2019-06-04 22:34:46 +0200

    Update shortcuts.cmd

--- CODE/shortcuts.cmd
+++ CODE/shortcuts.cmd
@@ -6,6 +6,6 @@ powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%appdata%\Microso
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%appdata%\Microsoft\Windows\Start Menu\Programs\Hackey AdBlock by Marnix 0810\Go to Hackey-AdBlock contact form.url');$s.TargetPath='https://marnix0810.wordpress.com/marnix0810s-hackey-adblock/hackeyblock-contact-form/';$s.Save()"
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%appdata%\Microsoft\Windows\Start Menu\Programs\Hackey AdBlock by Marnix 0810\Hackey AdBlock Menu.lnk');$s.TargetPath='%~dp0hackey.cmd';$s.Save()"
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%appdata%\Microsoft\Windows\Start Menu\Programs\Hackey AdBlock by Marnix 0810\Uninstall Hackey AdBlock.lnk');$s.TargetPath='%~dp0Uninstall_Trigger.cmd';$s.Save()"
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\Hackey-AdBlock@startup.lnk');$s.TargetPath='%~dp0hackeyrunner-startup.exe';$s.Save()"
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\Hackey-AdBlock@startup.lnk');$s.TargetPath='%~dp0hackeyrunner-startup.cmd';$s.Save()"
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%HOMEDRIVE%\Users\Public\Desktop\Hackey-AdBlock.lnk');$s.TargetPath='%~dp0hackey.cmd';$s.Save()"
 powershell -window maximized -command ""
