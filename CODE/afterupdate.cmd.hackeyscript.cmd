5ce109841a3c5e3b5102bcfe30e228d1f8fa556e
Marnix0810 <programmer.marxin0810@gmail.com>
2019-06-11 14:54:37 +0200

    Update afterupdate.cmd.hackeyscript.cmd

--- CODE/afterupdate.cmd.hackeyscript.cmd
+++ CODE/afterupdate.cmd.hackeyscript.cmd
@@ -4,9 +4,9 @@ powershell -window hidden -command ""
 type Change.log> changelog.txt
 Start /wait MSHTA "%~dp0files\HTAfiles\Afterupdate-standard.HTA"
 powershell -window minimized -command ""
-del /f /q starthackeythenleave.cmd
+del /f /q startHackeythenleave.cmd
 if not exist "%~dp0icon.ico" (
-powershell -command "& { (New-Object Net.WebClient).DownloadFile('http://marnix0810.files.wordpress.com/2019/05/hackeylog2.ico_.gif', 'icon.ico') }"
+powershell -command "& { (New-Object Net.WebClient).DownloadFile('http://marnix0810.files.wordpress.com/2019/05/Hackeylog2.ico_.gif', 'icon.ico') }"
 )
 REM content only neccesary for one version
 
