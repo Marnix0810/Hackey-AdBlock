864f413374e97f0a6fc22598044ab12314f3f5fa
Marnix0810 <programmer.marxin0810@gmail.com>
2019-07-06 15:15:18 +0200

    Aplied HARR to the Hackey Scripts

--- CODE/hackeyblock.cmd
+++ CODE/hackeyblock.cmd
@@ -1,22 +1,9 @@
 @ECHO OFF
 color F0
 call powershell -window maximize -command ""
-:: BatchGotAdmin
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-
-if '%errorlevel%' NEQ '0' (
-    echo Requesting administrative privileges...
-    goto UACPrompt
-) else ( goto gotAdmin )
-
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    set params = %*:"="
-    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-
-    "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
-    exit /B
+if not "%Adminrequested%"=="1" (
+call HARR "%~s0"
+)
 
 :gotAdmin
     pushd "%CD%"
--- CODE/hackeyrunner-startup.cmd
+++ CODE/hackeyrunner-startup.cmd
@@ -3,23 +3,10 @@ if "%Hackeyonstartup-timeout%"=="Not" exit
 cls
 color F0
 call powershell -window maximize -command ""
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if not "%Adminrequested%"=="1" (
+call HARR "%~s0"
+)
 
-if '%errorlevel%' NEQ '0' (
-    echo Requesting administrative privileges for HackeyBlock...
-    goto UACPrompt
-) else ( goto gotAdmin )
-
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    set params = %*:"="
-    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-
-    "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
-    exit /B
-
-:gotAdmin
     pushd "%CD%"
     CD /D "%~dp0"
 call "%~dp0loadset.cmd"
