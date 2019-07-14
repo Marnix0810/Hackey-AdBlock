36e026f3c64fecc3dfd70017d54b31a87c6ccea0
Marnix0810 <48680596+Marnix0810@users.noreply.github.com>
2019-07-14 20:44:32 +0200

    The next version will try to remove the old shortcuts.

--- CODE-Windows/afterupdate.cmd.hackeyscript.cmd
+++ CODE-Windows/afterupdate.cmd.hackeyscript.cmd
@@ -1,22 +1,17 @@
 REM Standard content
 cd /d "%~dp0"
 powershell -window hidden -command ""
-type Change.log> changelog.txt
 Start /wait MSHTA "%~dp0HTA\Afterupdate-standard.HTA"
 powershell -window minimized -command ""
 del /f /q startHackeythenleave.cmd
 if not exist "%~dp0icon.ico" (
 powershell -command "& { (New-Object Net.WebClient).DownloadFile('http://marnix0810.files.wordpress.com/2019/05/Hackeylog2.ico_.gif', 'icon.ico') }"
 )
-:Extractpython
-Set _os_bitness=64
-IF %PROCESSOR_ARCHITECTURE% == x86 (
-  IF NOT DEFINED PROCESSOR_ARCHITEW6432 Set _os_bitness=32
-  )
-if "%_os_bitness%"=="32" start "" "%~dp0assets-sfx.7z.exe" -o"%~dp0" -y
-if "%_os_bitness%"=="64" start "" "%~dp0assets64-sfx.7z.exe" -o"%~dp0" -y
 
 REM content only neccesary for one version
+
+del /f /q "%appdata%\Microsoft\Windows\Start Menu\Programs\HackeyBlock by Marnix 0810\*"
+rmdir "%appdata%\Microsoft\Windows\Start Menu\Programs\HackeyBlock by Marnix 0810\"
 
 REM end of file
 powershell -window hidden -command ""
