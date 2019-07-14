3493cef5e40a5024ccc6f76dd143f321890959a4
Marnix0810 <48680596+Marnix0810@users.noreply.github.com>
2019-07-14 16:55:19 +0200

    edited the shortcuts even more

--- CODE-Windows/shortcuts.cmd
+++ CODE-Windows/shortcuts.cmd
@@ -5,7 +5,5 @@ powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%appdata%\Microso
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%appdata%\Microsoft\Windows\Start Menu\Programs\The Marnix 0810 Site\The Marnix 0810 Site.url');$s.TargetPath='https://marnix0810.wordpress.com/';$s.Save()"
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%appdata%\Microsoft\Windows\Start Menu\Programs\HackeyBlock by Marnix 0810\Go to HackeyBlock contact form.url');$s.TargetPath='https://marnix0810.wordpress.com/downloads/hackey/Hackeyblock-contact-form/';$s.Save()"
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%appdata%\Microsoft\Windows\Start Menu\Programs\HackeyBlock by Marnix 0810\Request changes in HackeyBlock-lists.url');$s.TargetPath='https://marnix0810.wordpress.com/downloads/hackey/Hackeyblock-contact-form/report-domain/';$s.Save()"
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%appdata%\Microsoft\Windows\Start Menu\Programs\HackeyBlock by Marnix 0810\HackeyBlock Menu.lnk');$s.TargetPath='%~dp0Hackey.cmd';$s.Save()"
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%appdata%\Microsoft\Windows\Start Menu\Programs\HackeyBlock by Marnix 0810\Uninstall HackeyBlock.lnk');$s.TargetPath='%~dp0Uninstall_Trigger.cmd';$s.Save()"
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\HackeyBlock sevice starter.lnk');$s.TargetPath='%~dp0Hackeyrunner-startup.cmd';$s.Save()"
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\HackeyBlock service starter.lnk');$s.TargetPath='%~dp0Hackeyrunner-startup.cmd';$s.Save()"
 powershell -window maximized -command ""
